5c8d80c7539913a7a06bba1bc3f4cb00a27e0952
mohit <mosarma@gmail.com>
2018-06-05 20:45:05 +0530

    initial commit
    initial commit

---
+++ testinglibrary/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Configuration;
+using System.Reflection;
+
+
+namespace testinglibrary
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public static void test()
+        {
+            int a = 30;
+
+            var testname = Properties.names1.Default.Properties;
+            Console.WriteLine("wed");
+            List<string> names = new List<string >();
+            foreach (SettingsProperty currentProperty in Properties.names1.Default.Properties)
+            {
+                names.Add(currentProperty.Name);
+             
+            }
+
+             //   Properties.names1.Default.name1;
+             //  Console.ReadKey();
+        }
+    }
+}
